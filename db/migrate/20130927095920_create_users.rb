ed89ef64b7eca6f18590a71e4ccd27f44082842b
Jake Sikora <jakesikora.01@gmail.com>
2013-09-27 21:42:16 -0500

    Added model validations, added migration validations

--- db/migrate/20130927095920_create_users.rb
+++ db/migrate/20130927095920_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :email
-      t.string :password_hash
+      t.string :username, null: false
+      t.string :email, null: false
+      t.string :password_hash, null: false
       t.timestamps
     end
   end
